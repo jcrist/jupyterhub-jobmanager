b35d6ef90ddcff1b5aa172bb7ebc14dd31f45f99
Jim Crist <jiminy.crist@gmail.com>
2019-12-16 12:55:08 -0600

    Use versioneer

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup
 
+import versioneer
+
+install_requires = ["tornado"]
+
 setup(
     name="jupyterhub-jobmanager",
-    version="0.0.1",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     license="BSD",
     maintainer="Jim Crist",
     maintainer_email="jiminy.crist@gmail.com",
     description="Manage job deployments on JupyterHub",
     packages=["jupyterhub_jobmanager"],
     python_requires=">=3.5",
+    install_requires=install_requires,
     entry_points={
-        "console_scripts": [
-            "jupyterhub-jobmanager = jupyterhub_jobmanager.app:main",
-        ]
+        "console_scripts": ["jupyterhub-jobmanager = jupyterhub_jobmanager.app:main"]
     },
     zip_safe=False,
 )
